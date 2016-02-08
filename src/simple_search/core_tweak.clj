4d3d297836511c33407c858b72e08533cb758f66
chapm250 <chapm250@gmail.com>
2016-02-07 21:41:52 -0600

    added the tweak

---
+++ src/simple_search/core_tweak.clj
@@ -0,0 +1,85 @@
+(ns simple-search.core
+  (:use simple-search.knapsack-examples.knapPI_11_20_1000
+        simple-search.knapsack-examples.knapPI_13_20_1000
+        simple-search.knapsack-examples.knapPI_16_20_1000))
+
+;;; An answer will be a map with (at least) four entries:
+;;;   * :instance
+;;;   * :choices - a vector of 0's and 1's indicating whether
+;;;        the corresponding item should be included
+;;;   * :total-weight - the weight of the chosen items
+;;;   * :total-value - the value of the chosen items
+
+(defn included-items
+  "Takes a sequences of items and a sequence of choices and
+  returns the subsequence of items corresponding to the 1's
+  in the choices sequence."
+  [items choices]
+  (map first
+       (filter #(= 1 (second %))
+               (map vector items choices))))
+
+(defn random-answer
+  "Construct a random answer for the given instance of the
+  knapsack problem."
+  [instance]
+  (let [choices (repeatedly (count (:items instance))
+                            #(rand-int 2))
+        included (included-items (:items instance) choices)]
+    {:instance instance
+     :choices choices
+     :total-weight (reduce + (map :weight included))
+     :total-value (reduce + (map :value included))}))
+
+
+;;  (println (random-answer knapPI_16_20_1000_1))
+
+;;; It might be cool to write a function that
+;;; generates weighted proportions of 0's and 1's.
+
+(defn score
+  "Takes the total-weight of the given answer unless it's over capacity,
+   in which case we return 0."
+  [answer]
+  (if (> (:total-weight answer)
+         (:capacity (:instance answer)))
+    0
+    (:total-value answer)))
+
+(defn add-score
+  "Computes the score of an answer and inserts a new :score field
+   to the given answer, returning the augmented answer."
+  [answer]
+  (assoc answer :score (score answer)))
+
+(defn random-search
+  [instance max-tries]
+  (apply max-key :score
+         (map add-score
+              (repeatedly max-tries #(random-answer instance)))))
+
+;; (time (random-search knapPI_16_20_1000_1 1000000
+;; ))
+
+;; (defn random-place
+;;   [instance]
+;;   (let [choices (repeatedly (+ (rand-int 3) 1)
+;;                             #(rand-int 2))
+;;         included (included-items (:items instance) choices)]
+;;     {:instance instance
+;;      :choices choices
+;;      :total-weight (reduce + (map :weight included))
+;;      :total-value (reduce + (map :value included))}))
+
+;; (defn tweak-choice
+;;   [choice]
+
+
+;;  (println (random-place (random-answer knapPI_16_20_1000_1)))
+
+;; (println  (into [] (concat [(rand-int 3) (rand-int 3) (rand-int 3)] [3 4 5])))
+;; (println (replace [1 0 1 0 1 1] (into [] (concat [(rand-int 3) (rand-int 3) (rand-int 3)] [3 4 5])) ))
+(println (replace '(1 0 1 0 1 1) [1 2 3 4 5 6]))
+
+;; (defn hillClimb
+;;   [instance]
