30b24fc6c833cead92d255a04fe9fe6c4cf6e0a5
Josh Chapman <chapm250@morris.umn.edu>
2016-02-10 23:17:15 -0600

    Fixed off by one error, should probably update docs next

--- src/simple_search/core_tweak.clj
+++ src/simple_search/core_tweak.clj
@@ -32,14 +32,14 @@
      :total-value (reduce + (map :value included))}))
 
 
-  ;(println (random-answer knapPI_16_20_1000_1))
+;;  (println (random-answer knapPI_16_20_1000_1))
 
 ;;; It might be cool to write a function that
 ;;; generates weighted proportions of 0's and 1's.
 
 (defn score
   "Takes the total-weight of the given answer unless it's over capacity,
-   in which case we return 0."
+  in which case we return 0."
   [answer]
   (if (> (:total-weight answer)
          (:capacity (:instance answer)))
@@ -48,7 +48,7 @@
 
 (defn add-score
   "Computes the score of an answer and inserts a new :score field
-   to the given answer, returning the augmented answer."
+  to the given answer, returning the augmented answer."
   [answer]
   (assoc answer :score (score answer)))
 
@@ -57,6 +57,7 @@
   (apply max-key :score
          (map add-score
               (repeatedly max-tries #(random-answer instance)))))
+
 
 (defn find-answer
   [choice instance]
@@ -67,38 +68,16 @@
      :total-weight (reduce + (map :weight included))
      :total-value (reduce + (map :value included))}))
 
-
-
-
 (defn tweak-choice
   [choice x]
   (let [conchoice (into [] choice)]
     (if (> x 0)
-      (tweak-choice (assoc conchoice (rand-int (count conchoice)) (rand-int 1)) (dec x))
+      (tweak-choice (assoc conchoice (rand-int (count conchoice)) (rand-int 2)) (dec x))
       (seq choice)
       )))
 
-;;  (defn tweak-choice
-;;    [choice x]
-;;    (let [conchoice (into [] choice)]
-;;      (if (> x 0)
-;;        (let [index (rand-int (count conchoice))]
-;;          (tweak-choice (assoc conchoice index (mod (+ 1 (nth conchoice index)) 2)) (dec x))
-;;          (seq choice)
-;;          ))))
-
-;(println (tweak-choice (:choices (random-answer knapPI_16_20_1000_1))  3))
-(tweak-choice '(1 1 1 1 1)  3)
-;(max-key {:score 10} {:score 12})
-
-(add-score (find-answer (tweak-choice (:choices (random-answer knapPI_11_20_1000_1)) 2) knapPI_11_20_1000_1)
-)
-
-(mod 7 2)
-
 (defn hill-climb
   [winner max-tries instance]
-  ; (println "winner")
   (loop [num-tries 0
          current-best winner]
     (if (>= num-tries max-tries)
@@ -106,16 +85,9 @@
       (let [tweaked-choices (tweak-choice (:choices current-best)  10)
             new-answer (find-answer tweaked-choices instance)
             scored-new-answer (add-score new-answer)]
-         (println scored-new-answer)
         (recur (inc num-tries)
                (max-key :score current-best scored-new-answer))))))
 
-(let [start (add-score (random-answer knapPI_16_20_1000_74))
-      hill-best (hill-climb start 200 knapPI_16_20_1000_74)]
-  (println start)
-  hill-best
-)
 
-(random-search knapPI_16_20_1000_56 2000)
-
-
+(hill-climb (add-score (random-answer knapPI_11_20_1000_1)) 20000 knapPI_11_20_1000_1
+            )
